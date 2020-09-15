59b6a510202a0025ba3c66d0c4e2017256f9d2e6
Mcuser <Mcuser@LAB1-PC54>
2020-09-15 12:06:30 +0200

    importing project

---
+++ ShoppingCart_Tut/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using ShoppingCart_Tut.Models;
+
+namespace ShoppingCart_Tut.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("MyDbContext")
+        {
+
+        }
+
+        public virtual DbSet<Product> products { get; set; }
+    }
+}
